5acc31fbf39be0afe2199a474d019dccc771e11c
Carlos Banks <cbanks@clypr.co>
2020-11-01 20:29:11 -0500

    refactored ls8.py and completed multi in cpu.py

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,5 +1,3 @@
-"""CPU functionality."""
-
 import sys
 
 class CPU:
@@ -7,38 +5,115 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.pc = 0
+        self.sp = 7  # pointer location in register
+        self.reg[self.sp] = 0xF3  # Slot in memory 243
+        self.fl = 0b00000000 # Flags Register
+        self.running = False
+        self.instr = {
+            0b10000010: self.LDI,
+            0b10100010: self.MUL,
+            0b01000111: self.PRN,
+            0b00000001: self.HLT,
+            # 0b01000101: self.PUSH,
+            # 0b01000110: self.POP,
+            # 0b01010000: self.CALL,
+            # 0b00010001: self.RET,
+            0b10100000: self.ADD,
+            0b10100111: self.CMP,
+            0b01010101: self.JEQ,
+            0b01010110: self.JNE,
+            0b01010100: self.JMP
+        }
+
+    def JMP(self, op1=None, op2=None):
+        self.pc = self.reg[op1]
+
+    def JNE(self, op1=None, op2=None):
+        if (self.fl & 0b00000001) == 0:
+            self.pc = self.reg[op1]
+        else:
+            self.pc += 2
+
+    def JEQ(self, op1=None, op2=None):
+        if self.fl & 0b00000001 == 1:
+            self.pc = self.reg[op1]
+        else:
+            self.pc += 2
+
+    def ram_read(self, op1=None):
+        return self.ram[op1]
+
+    def ram_write(self, op1=None, op2=None):
+        self.ram[op1] = op2
+
+    def HLT(self, op1=None, op2=None):
+        self.running = False
+        self.pc += 1
+        sys.exit()
+
+    def LDI(self, op1=None, op2=None):
+        reg_num = self.ram[self.pc + 1]
+        value = self.ram[self.pc + 2]
+        self.reg[reg_num] = value
+        self.pc += 3
+
+    def PRN(self, op1=None, op2=None):
+        print(self.reg[op1])
+        self.pc += 2
+
 
     def load(self):
         """Load a program into memory."""
 
         address = 0
+        if len(sys.argv) != 2:
+            print("no file given to run")
+        try:
+            with open(sys.argv[1], "r") as file:
+                program = file.readlines()
 
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+                for instruction in program:
+                    if instruction.startswith("#"):
+                        continue
+                    split_inst = instruction.split(' ')[0]
+                    stripped_inst = split_inst.strip()
+                    if stripped_inst == '':
+                        continue
+                    self.ram[address] = int(stripped_inst, 2)
+                    address += 1
+        except FileNotFoundError:
+            print(f"could not find file {sys.argv[1]}")
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "CMP":
+            if self.reg[reg_a] == self.reg[reg_b]:
+                self.fl = 0b00000001
+            elif self.reg[reg_a] > self.reg[reg_b]:
+                self.fl = 0b00000010
+            elif self.reg[reg_a] < self.reg[reg_b]:
+                self.fl = 0b00000100
+
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
+        self.pc += 3
+
+    def MUL(self, op1=None, op2=None):
+        self.alu("MUL", op1, op2)
+
+    def ADD(self, op1=None, op2=None):
+        self.alu("ADD", op1, op2)
+
+    def CMP(self, op1=None, op2=None):
+        self.alu("CMP", op1, op2)
 
     def trace(self):
         """
@@ -62,4 +137,11 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        self.running = True
+        while self.running:
+            instruction = self.ram_read(self.pc)
+            if instruction in self.instr:
+                self.instr[instruction](self.ram_read(self.pc + 1), self.ram_read(self.pc + 2))
+            else:
+                print("Unknown Instruction Command")
+                self.HLT()
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -1,22 +1,9 @@
-#!/usr/bin/env python3
-
 """Main."""
 
 import sys
 from cpu import *
 
-def main(argv):
+cpu = CPU()
 
-    if len(argv) != 2:
-        print("usage: .ls8 filename")
-        return 1
-
-    cpu = CPU()
-
-    cpu.load(argv[1])
-        cpu.run()
-
-        return 0
-
-if __name__ == "__main__":
-    sys.exit(main(sys.argv))
+cpu.load()
+cpu.run()
